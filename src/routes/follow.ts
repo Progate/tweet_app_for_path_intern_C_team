8df3a695cbd775b03141776be15fe38585674470
MAO-business <124860107+MAO-business@users.noreply.github.com>
2023-08-29 03:14:04 +0000

    フォローリクエストしたときのURLを更新

--- src/routes/follow.ts
+++ src/routes/follow.ts
@@ -1,31 +1,36 @@
 import express from "express";
 import {ensureAuthUser} from "@/middlewares/authentication";
+import {getPost} from "@/models/post";
 import {createFollow, deleteFollow} from "@/models/follow";
 
 export const followRouter = express.Router();
 
-followRouter.post("/:postId/follows/:userId", ensureAuthUser, async (req, res, next) => {
+followRouter.post("/:postId/follow", ensureAuthUser, async (req, res, next) => {
   const {postId} = req.params;
-  const {userId} = req.params;
+  const post = await getPost(Number(postId));
+  if (!post || !post.id)
+    return next(new Error("Invalid error: The post or post.id is undefined."));
   const currentUserId = req.authentication?.currentUserId;
   if (currentUserId === undefined) {
     // `ensureAuthUser` enforces `currentUserId` is not undefined.
     // This must not happen.
     return next(new Error("Invalid error: currentUserId is undefined."));
   }
-  await createFollow({followingId: currentUserId, followedId: Number(userId)});
+  await createFollow({followingId: currentUserId, followedId: post.userId});
   res.redirect(`/posts/${postId}`);
 });
 
-followRouter.delete("/:postId/follows/:userId", ensureAuthUser, async (req, res, next) => {
+followRouter.delete("/:postId/unfollow", ensureAuthUser, async (req, res, next) => {
   const {postId} = req.params;
-  const {userId} = req.params;
+  const post = await getPost(Number(postId));
+  if (!post || !post.id)
+    return next(new Error("Invalid error: The post or post.id is undefined."));
   const currentUserId = req.authentication?.currentUserId;
   if (currentUserId === undefined) {
     // `ensureAuthUser` enforces `currentUserId` is not undefined.
     // This must not happen.
     return next(new Error("Invalid error: currentUserId is undefined."));
   }
-  await deleteFollow({followingId: currentUserId, followedId: Number(userId)});
+  await deleteFollow({followingId: currentUserId, followedId: post.userId});
   res.redirect(`/posts/${postId}`);
 });
