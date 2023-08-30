ad64bfb8f59c0ad849726afbf7eaa99da570be88
MAO-business <124860107+MAO-business@users.noreply.github.com>
2023-08-30 03:24:57 +0000

    conflictを解消

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -72,7 +72,7 @@ userRouter.get("/:userId", ensureAuthUser, async (req, res, next) => {
   const FollowerCount = await getFollowerCount(Number(userId));
   if (!userTimeline)
     return next(new Error("Invalid error: The user is undefined."));
-  
+
   const currentUserId = req.authentication?.currentUserId;
   if (currentUserId === undefined) {
     // `ensureAuthUser` enforces `currentUserId` is not undefined.
@@ -217,14 +217,21 @@ userRouter.post("/:userId/follow", ensureAuthUser, async (req, res, next) => {
   res.redirect(`/users/${userId}`);
 });
 
-userRouter.delete("/:userId/unfollow", ensureAuthUser, async (req, res, next) => {
-  const {userId} = req.params;
-  const currentUserId = req.authentication?.currentUserId;
-  if (currentUserId === undefined) {
-    // `ensureAuthUser` enforces `currentUserId` is not undefined.
-    // This must not happen.
-    return next(new Error("Invalid error: currentUserId is undefined."));
+userRouter.delete(
+  "/:userId/unfollow",
+  ensureAuthUser,
+  async (req, res, next) => {
+    const {userId} = req.params;
+    const currentUserId = req.authentication?.currentUserId;
+    if (currentUserId === undefined) {
+      // `ensureAuthUser` enforces `currentUserId` is not undefined.
+      // This must not happen.
+      return next(new Error("Invalid error: currentUserId is undefined."));
+    }
+    await deleteFollow({
+      followingId: currentUserId,
+      followedId: Number(userId),
+    });
+    res.redirect(`/users/${userId}`);
   }
-  await deleteFollow({followingId: currentUserId, followedId: Number(userId)});
-  res.redirect(`/users/${userId}`);
-});
+);
